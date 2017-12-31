e2d1927551f2314cfcf8a4b9e7bfd23fc4d292a0
Felipe Guerra <fguerra0310@gmail.com>
2017-12-31 00:12:50 -0200

    Espelhando o Banco de Dados.

--- Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/Movie.java
+++ Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/Movie.java
@@ -1,5 +1,39 @@
 package com.schoolofnet.Java_Jdbc;
 
 public class Movie {
+	
+	private Integer id;
+	
+	private String name;
+	
+	public Movie() {
+		
+	}
+	
+	public Movie(String name) {
+		this.name = name;
+	}
+	
+	public Movie(Integer id, String name) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
