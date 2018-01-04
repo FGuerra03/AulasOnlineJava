ec10ccac564ddae705618516e94f0735443bfb7f
Felipe Guerra <fguerra0310@gmail.com>
2018-01-04 20:01:23 -0200

    Java Swing, Hello World

---
+++ App/src/com/fguerra/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.fguerra;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class HelloWorld {
+	
+	private static JFrame frame;
+	private static JLabel lblHello;
+	
+	public static void main(String[] args) {
+		frame = new JFrame();
+		lblHello = new JLabel("Hello World from Java Swing", SwingConstants.CENTER);
+		frame.setBounds(100, 100, 250, 200);
+		
+		frame.add(lblHello);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("First Frame");
+		frame.setVisible(true);
+	}
+}
