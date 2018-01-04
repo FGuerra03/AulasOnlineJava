c915aef05e0445e24e2190c672ac1f49009111e4
Felipe Guerra <fguerra0310@gmail.com>
2018-01-03 22:21:34 -0200

    Trabalhando com DAO

--- Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/App.java
+++ Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/App.java
@@ -11,18 +11,18 @@ public class App
     public static void main( String[] args ) throws SQLException{
       Scanner scanner = new Scanner(System.in);
       System.out.println("---------- MENU ---------");
-      System.out.println("1 - Listar filmes");
-      System.out.println("2 - Adicionar novo filme");
+      System.out.println("1 - Adicionar novo filme");
+      System.out.println("2 - Listar Filmes");
       System.out.println("3 - Atualizar um filme");
       System.out.println("4 - Excluir um filme");
       int choice = scanner.nextInt();
       switch (choice) {
       	case 1:
-      		System.out.println("Filmes: ");
+      		System.out.println("Adicionado");
       		Connection con = new ConnectionFactory().getConnection();
       		break;
       	case 2:
-      		System.out.println("Adicionado");
+      		System.out.println("Filmes: ");
       		break;
       	case 3:
       		System.out.println("Atualizado");
