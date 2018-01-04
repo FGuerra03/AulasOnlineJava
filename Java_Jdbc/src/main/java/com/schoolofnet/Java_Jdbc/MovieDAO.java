d14a39b0bad2b1a23881deb83483fb8232f134d8
Felipe Guerra <fguerra0310@gmail.com>
2018-01-03 22:29:38 -0200

    Finalizando trabalhos com DAO

--- Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/MovieDAO.java
+++ Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/MovieDAO.java
@@ -1,5 +1,53 @@
 package com.schoolofnet.Java_Jdbc;
 
-public class MovieDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MovieDAO {
+	
+	private Connection connection = null;
+	private PreparedStatement statement = null;
+	
+	
+	public MovieDAO() {
+		connection = new ConnectionFactory().getConnection();
+	}
+	
+	public List<Movie> findAll() throws SQLException{
+		String query = "SELECT * FROM movie";
+		List<Movie> movies = new ArrayList<Movie>();
+		
+		statement = connection.prepareStatement(query);
+		ResultSet res = statement.executeQuery();
+		while(res.next()) {
+			movies.add(new Movie(res.getInt("id"), res.getString("name")));
+		}
+		
+		return movies;
+	}
+	
+	public void insert(Movie movie) throws SQLException {
+		String query = "INSERT INTO movie (name) VALUES(?)";
+		statement = connection.prepareStatement(query);
+		statement.setString(1, movie.getName());
+		statement.execute();
+	}
+	
+	public void update(Movie movieOld, Movie movieNew) throws SQLException {
+		String query = "UPDATE movie SET name = ? WHERE id =?";
+		statement = connection.prepareStatement(query);
+		statement.setString(1, movieNew.getName());
+		statement.setInt(2, movieOld.getId());
+	}
+	
+	public void remove(Movie movie) throws SQLException {
+		String  query = "DELETE FROM movie WHERE id = ?";
+		statement = connection.prepareStatement(query);
+		statement.setInt(1, movie.getId());
+		statement.execute();
+	}
 }
